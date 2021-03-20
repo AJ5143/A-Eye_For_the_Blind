65474cf1f2b5e740e2f2334e2ebe829aa3489875
saranggoel <64043281+saranggoel@users.noreply.github.com>
2021-03-20 15:30:15 -0500

    Some more updates

--- On-Person Device Code.py
+++ On-Person Device Code.py
@@ -1,6 +1,9 @@
 from firebase import Firebase
 import sys
-import requests
+from datetime import datetime
+import geocoder
+g = geocoder.ip('me')
+print(g.latlng)
 config = {
     "apiKey": "AIzaSyBkQ5z8Rs5IRP4lHoiWyXV9XVQHjAh-sEI",
     "authDomain": "a-eye-for-the-blind.firebaseapp.com",
@@ -15,6 +18,11 @@ db = firebase.database()
 auth = firebase.auth()
 storage = firebase.storage()
 
+def time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
+
 if uid == "" or email == "" or password == "":
     print("Please set user UID, email, or password in the lines above!")
     sys.exit()
@@ -23,7 +31,8 @@ user = auth.sign_in_with_email_and_password(email, password)
 user = auth.refresh(user['refreshToken'])
 data = {"3": "Joe Tilsed"}
 db.child(f"users/{uid}").child("images").update(data)
-storage.child("images/example.jpg").put("D:/A-Eye For The Blind/2dtodepth/outfile/test_image.jpg", user['idToken'])
-storage.child("images/example.jpg").download("D:/A-Eye For The Blind/2dtodepth/outfile/downloaded.jpg", user['idToken'])
+storage.child(f"images/{uid} {time()}.jpg").put("D:/A-Eye For The Blind/2dtodepth/outfile/test_image.jpg", user['idToken'])
+storage.child("images/example1.jpg").put("D:/A-Eye For The Blind/2dtodepth/outfile/test_image.jpg", user['idToken'])
+
 
 
